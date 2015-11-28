cd2a2e64466c0d4f6fae26953e9b83744f4e790c
Aaron Bonner <aaron_bonner11@hotmail.com>
2015-11-28 17:22:40 -0400

    Added verticies, colors, and indicies

--- index.js
+++ index.js
@@ -3,6 +3,37 @@ function main() {
 	window.gl = context.gl;
 	gl.clearColor(0, 0, 0, 1);
 	console.log(context);
+	getObjectResources(['shaders/shader1.vshader','shaders/shader1.fshader'],function(resourceArray){
+		var vertices = new Float32Array([   // Vertex coordinates
+			1.0, 1.0, 1.0,  -1.0, 1.0, 1.0,  -1.0,-1.0, 1.0,   1.0,-1.0, 1.0,  // v0-v1-v2-v3 front
+			1.0, 1.0, 1.0,   1.0,-1.0, 1.0,   1.0,-1.0,-1.0,   1.0, 1.0,-1.0,  // v0-v3-v4-v5 right
+			1.0, 1.0, 1.0,   1.0, 1.0,-1.0,  -1.0, 1.0,-1.0,  -1.0, 1.0, 1.0,  // v0-v5-v6-v1 up
+			-1.0, 1.0, 1.0,  -1.0, 1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0,-1.0, 1.0,  // v1-v6-v7-v2 left
+			-1.0,-1.0,-1.0,   1.0,-1.0,-1.0,   1.0,-1.0, 1.0,  -1.0,-1.0, 1.0,  // v7-v4-v3-v2 down
+			1.0,-1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0, 1.0,-1.0,   1.0, 1.0,-1.0   // v4-v7-v6-v5 back
+		]);
+
+		var colors = new Float32Array([     // Colors
+			0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  // v0-v1-v2-v3 front(blue)
+			0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  // v0-v3-v4-v5 right(green)
+			1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  // v0-v5-v6-v1 up(red)
+			1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  // v1-v6-v7-v2 left
+			1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  // v7-v4-v3-v2 down
+			0.4, 1.0, 1.0,  0.4, 1.0, 1.0,  0.4, 1.0, 1.0,  0.4, 1.0, 1.0   // v4-v7-v6-v5 back
+		]);
+
+		var indices = new Uint8Array([       // Indices of the vertices
+			0, 1, 2,   0, 2, 3,    // front
+			4, 5, 6,   4, 6, 7,    // right
+			8, 9,10,   8,10,11,    // up
+			12,13,14,  12,14,15,    // left
+			16,17,18,  16,18,19,    // down
+			20,21,22,  20,22,23     // back
+		]);
+		console.log(resourceArray);
+		context.addGLObject(new ship(resourceArray[0],resourceArray[1],verticies,colors,indexes));
+	});
+	
 	var tick = function() {
 		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 		for(i=0;i<context.glObjects.length;i++){
@@ -26,7 +57,7 @@ function glContext(canvasID){
 	}
 }
 
-function ship(vertexShaderSource,fragmentShaderSource,vertexArray,vertexColorsArray,elementIndexArray,callback){
+function ship(vertexShaderSource,fragmentShaderSource,vertexArray,vertexColorsArray,elementIndexArray){
 	this.last_update = Date.now();
 	this.initialized = false;
 	this.init = function(){
