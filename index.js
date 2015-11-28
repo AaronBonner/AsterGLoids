139dbcd29a5787b0e950ab71e56d7c78f9aad6b6
Aaron Bonner <aaron_bonner11@hotmail.com>
2015-11-28 15:22:18 -0400

    Back to the drawing board. Beginning major rewrite

--- index.js
+++ index.js
@@ -1,67 +1,44 @@
 function main() {
-	var canvas = document.getElementById('webgl');
-	var gl = getWebGLContext(canvas);
+	window.context = new glContext("webgl");
+	window.gl = context.gl;
+	gl.clearColor(0, 0, 0, 1);
+	console.log(context);
+	var tick = function() {
+		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+		for(i=0;i<context.glObjects.length;i++){
+			context.glObjects[i].draw();
+		}
+		requestAnimationFrame(tick, context.canvas);
+	};
+	tick();
+}
+
+function glContext(canvasID){
+	this.glObjects = [];
+	this.canvas = document.getElementById(canvasID);
+	this.gl = getWebGLContext(this.canvas);
+	console.log(this.gl);
 	
-	initGL(gl,drawShip,[]);
+	this.addGLObject = function(glObj){
+		console.log('adding gl object:');
+		console.log(glObj);
+		this.glObjects.push(glObj);
+	}
 }
 
-function getShadersFromXHR(vertexShaderURL,fragmentShaderURL,callback) {
-	var VSHADER_SOURCE = '';
-	var FSHADER_SOURCE = '';
-
-	$.ajax({
-		url: vertexShaderURL,
-		dataType: "text",
-		cache: false
-	}).done(function(data) {
-		VSHADER_SOURCE = data;
-		$.ajax({
-			url: fragmentShaderURL,
-			dataType: "text",
-			cache: false
-		}).done(function(data) {
-			FSHADER_SOURCE = data;
-			shaders = {'VSHADER_SOURCE':VSHADER_SOURCE,'FSHADER_SOURCE':FSHADER_SOURCE};
-			console.log(shaders);
-			callback(shaders);
-		});
-	});
-}
-
-function initGL(gl,callback,callbackArgs) {
-	if (!gl) {
-		console.log('Failed to get the rendering context for WebGL');
-		return;
+function ship(vertexShaderUrl,fragmentShaderUrl,vertexArray,vertexColorsArray,elementIndexArray,callback){
+	this.last_update = Date.now();
+	this.initialized = false;
+	this.init = function(){
+		console.log('heres where we init the shaders, buffers, and other things');
 	}
 	
-	var VSHADER_SOURCE = '';
-	var FSHADER_SOURCE = '';
-	
-	getShadersFromXHR('shaders/shader1.vshader','shaders/shader1.fshader',function (shaders){
-		VSHADER_SOURCE = shaders['VSHADER_SOURCE'];
-		FSHADER_SOURCE = shaders['FSHADER_SOURCE'];
-		console.log('gotshaders');
-		
-		if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
-			console.log('Failed to initialize shaders.');
-			return;
-		}
-		
-		console.log('vshader:');
-		console.log(VSHADER_SOURCE);
-		console.log('fshader:');
-		console.log(FSHADER_SOURCE);
-		
-		gl.clearColor(0.0, 0.0, 0.0, 1.0);
+	this.draw = function(){
+		this.animate(Date.now() - this.last_update);
+		console.log('here is where things would be drawn');
+	};
 
-		gl.clear(gl.COLOR_BUFFER_BIT);
-		
-		return callback.apply(this,callbackArgs);
-	});
-}
-
-
-
-function drawShip(gl){
-	console.log('stuff goes here');
+	this.animate = function(elapsed){
+		console.log('Elapsed time is ' + elapsed.toString());
+	}
 }
